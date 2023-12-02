c92bd1fd06d304c7a9f1b9bf9e56eaacf79b3362
Kaitlin Davis <niltiaksivad@gmail.com>
2023-12-02 08:11:42 -0700

    Initial Commit

---
+++ AdventOfCode2023/AdventOfCode2023App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2023App.swift
+//  AdventOfCode2023
+//
+//  Created by Kaitlin Davis on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
